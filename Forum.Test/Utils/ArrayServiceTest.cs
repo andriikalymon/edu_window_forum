71f710c27bf477a1333d32042f736090f6abe509
andriikalymon <andrik14wot@gmail.com>
2021-06-21 16:54:19 +0300

    Utils tests added

---
+++ Forum.Test/Utils/ArrayServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using Forum.Web.Utils;
+using Xunit;
+
+namespace Forum.Test.Utils
+{
+    public class ArrayServiceTest
+    {
+        [Theory]
+        [AutoData]
+        public void ConcatArray_IfOptionsIsSpecified_ReturnsArrayWithValue(string[] array, string[] optional)
+        {
+            // Arrange 
+            var firstOfOptional = optional[0];
+
+            // Act
+            var actualResult = ArrayService.ConcatArray(array, optional);
+
+            // Assert
+            actualResult.Should().Contain(firstOfOptional);
+        }
+
+        [Theory]
+        [AutoData]
+        public void ConcatArray_IfArrayIsSpecified_ReturnsArrayWithValue(string[] array, string[] optional)
+        {
+            // Arrange 
+            var firstOfArray = array[0];
+
+            // Act
+            var actualResult = ArrayService.ConcatArray(array, optional);
+
+            // Assert
+            actualResult.Should().Contain(firstOfArray);
+        }
+    }
+}
---
+++ Forum.Test/Utils/SearchServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Forum.Web.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Forum.Test.Utils
+{
+    public class SearchServiceTest
+    {
+        [Theory]
+        [InlineData("name", new string[] { "val", "surname" })]
+        public void Match_IfIsRelevant_ReturnTrue(string value, string[] options)
+        {
+            // Act
+            var result = SearchService.Match(value, options);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("age", new string[] { "val", "surname" })]
+        public void Match_IfIsNotRelevant_ReturnFalse(string value, string[] options)
+        {
+            // Act
+            var result = SearchService.Match(value, options);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+    }
+}
